071ff1549109d2e39517b086a033fb9914182407
toptraverseblog-jpg <toptraverse.blog@gmail.com>
2025-07-21 10:44:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import componentTagger from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/top-traverse-adventures-south-africa/", // <-- Add this line
+
   server: {
-    host: "::",
+    host: "*",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
